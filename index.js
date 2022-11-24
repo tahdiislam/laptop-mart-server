97cd327d53526ad62e3625834cc22f73e1e1363c
tahdi-islam <tshdi60@gmail.com>
2022-11-24 09:08:21 +0600

    add vercel config

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.get("/", (req, res) => {
   res.send("Laptop Mart server is running");
 });
 
+// connect to database(mongoDb)
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.rgyxe1r.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.listen(port, () => {
   console.log(`Laptop server is running port ${port}`);
 });
