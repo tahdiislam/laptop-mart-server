ffba6eb63d06658a6cc556dcf0f78deb9b6e054e
tahdi-islam <tshdi60@gmail.com>
2022-11-24 09:48:16 +0600

    post user to the data base

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,15 +16,29 @@ app.get("/", (req, res) => {
 
 // connect to database(mongoDb)
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.rgyxe1r.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rgyxe1r.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const Users = client.db("laptopMart").collection("usersCollection");
+
+    // post new user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await Users.insertOne(user);
+      res.send({ result });
+    });
+  } finally {
+  }
+}
+
+run().catch(console.log);
 
 app.listen(port, () => {
   console.log(`Laptop server is running port ${port}`);
